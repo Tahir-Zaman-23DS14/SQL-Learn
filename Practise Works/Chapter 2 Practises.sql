e7ca3d5a22038505157b4664104fcd593f35fe3f
TAHIR ZAMAN <tahirzaman8585@outlook.com>
2026-02-04 11:40:33 +0500

    Update Chapter 2 Practises.sql

--- Practise Works/Chapter 2 Practises.sql
+++ Practise Works/Chapter 2 Practises.sql
@@ -2,9 +2,47 @@
 -- CHAPTER 2
 
 SELECT *
- FROM dbo.orders
+ FROM dbo.customers
 
  SELECT *
   FROM dbo.orders
 
-  SELECT
+
+SELECT 
+id,
+first_name
+FROM customers
+
+
+SELECT
+order_id,
+customer_id
+FROM orders
+
+SELECT
+first_name,
+country,
+score
+FROM customers
+
+
+SELECT * 
+FROM customers
+WHERE score != 0
+
+SELECT *
+FROM customers
+WHERE country = 'Germany'
+
+SELECT *
+ first_name,
+ country
+ FROM customers
+WHERE country = 'Germany'
+
+
+SELECT * FROM customers
+ORDER BY score ASC
+
+SELECT * FROM customers
+ORDER BY score DESC
