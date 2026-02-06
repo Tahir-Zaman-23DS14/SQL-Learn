9c5843a3adc40c1ca03c7de8a0fb83d11894951a
TAHIR ZAMAN <tahirzaman8585@outlook.com>
2026-02-06 16:43:11 +0500

    Update Chapter 2 Practises.sql

--- Practise Works/Chapter 2 Practises.sql
+++ Practise Works/Chapter 2 Practises.sql
@@ -82,10 +82,68 @@ FROM customers
 
 
 SELECT
-count(id) AS ID_for_Each_Country,
 country,
-sum(score) AS Each_country_total_Score
+sum(score) AS Each_country_total_Score,
+count(id) AS ID_for_Each_Country
+
 from customers
 GROUP BY country
 
+HAVING sum(score)>=850
+
 SELECT * FROM customers
+
+SELECT
+country,
+first_name,
+AVG(score) As average_score
+FROM customers
+GROUP BY country,first_name
+HAVING AVG(score)>=450
+
+SELECT
+country,
+AVG(score)
+FROM customers
+WHERE score ! = 0
+GROUP BY country
+HAVING AVG(score) >450
+
+-- Q1 Write a query to find each Country and the total number of students in that Country, but only include Countries having more than 3 students.
+-- Sort the result by total students in descending order.
+SELECT 
+country,
+count(id) AS total_student
+FROM customers
+GROUP BY country 
+HAVING count(id) >3 
+ORDER BY total_student DESC
+
+
+-- Q2 Write a query to display Country-wise average score, but only for Countries where the average score is greater than 70.
+--Order the result by average score from highest to lowest.
+
+
+
+SELECT 
+	country,
+	AVG(score) AS average_score
+FROM customers
+GROUP BY country
+HAVING AVG(score) > 70
+ORDER BY AVG(score) DESC
+
+
+-- Q3 Write a query to count how many students scored above 800 in each Country.
+--  Show only those Countries where this count is at least 2.
+
+SELECT 
+country,
+COUNT(id) AS Student_Score
+FROM customers
+WHERE score > 80
+GROUP BY country
+HAVING COUNT(id) >= 2
+
+-- Q4 Write a query to find the maximum score in each Country, but exclude Countries where the maximum score is below 60.
+--  Sort the output by maximum score ascending.
