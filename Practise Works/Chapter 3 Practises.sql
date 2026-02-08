48491270362e26ca72c0a722baa797347c51114d
TAHIR ZAMAN <tahirzaman8585@outlook.com>
2026-02-08 15:15:21 +0500

    Create Chapter 3 Practises.sql

---
+++ Practise Works/Chapter 3 Practises.sql
@@ -0,0 +1,42 @@
+--DDL > DATA DEFINATION LANGUAGE
+-- CREATE, ALTER , DROP
+
+/* Create a New table called persons
+with columns: id , person_name , birth,_date, and phone */
+
+
+CREATE TABLE persons (
+  id INT NOT NULL,
+  person_name VARCHAR(50) NOT NULL,
+  birth_date DATE ,
+  phone VARCHAR(11),
+  CONSTRAINT pk_persons PRIMARY KEY (id)
+)
+
+
+SELECT * FROM persons
+
+-- Add a new column called email to the persons table
+
+ALTER TABLE persons
+ADD email VARCHAR(50) NOT NULL
+
+
+SELECT * FROM persons
+
+-- Remove a column called phone to the persons table
+ALTER TABLE persons
+DROP COLUMN phone
+
+-- Add a new column called phone to the persons table
+
+ALTER TABLE persons
+ADD phone VARCHAR(15)
+
+-- DELETE Entire Table persons
+DROP TABLE persons
+
+
+
+
+
