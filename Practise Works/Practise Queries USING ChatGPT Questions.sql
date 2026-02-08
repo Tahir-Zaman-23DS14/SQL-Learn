988a20ecd11a693f4b994aac0f1a8950deb7f866
TAHIR ZAMAN <tahirzaman8585@outlook.com>
2026-02-08 15:39:16 +0500

    Update Practise Queries USING ChatGPT Questions.sql

--- Practise Works/Practise Queries USING ChatGPT Questions.sql
+++ Practise Works/Practise Queries USING ChatGPT Questions.sql
@@ -8,7 +8,7 @@ CREATE DATABASE Practise;
 
 USE Practise
 
-CREATE TABLE students (
+CREATE TABLE students(
 Id INT NOT NULL,
 First_Name VARCHAR(50) not null,
 Age  DATE  ,
@@ -16,6 +16,15 @@ Country VARCHAR(50),
 Score INT,
 Enrollment_date DATE
 CONSTRAINT pk_students PRIMARY KEY (Id)
-)
+);
 
-SELECT * FROM students
+DROP TABLE students;
+
+SELECT * FROM students;
+
+INSERT INTO students (id, first_name, age, country, score, enrollment_date)
+VALUES
+		(1, 'Tahir', '2003-09-06', 'Pakistan', 85, '2023-11-06'),
+		(2, 'Zaman', '1978-09-06', 'Pakistan', 95, '2003-11-06'),
+		(3, 'Muheed', '2026-09-06', 'Pakistan', 45, '2025-11-06');
+
